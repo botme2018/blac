df6cb4d90a4a2c4ba60ea22f0954fc7b28771ffd
botme2018 <botme2018@gmail.com>
2017-03-25 11:03:30 +0430

    Update MuteTime.lua

--- plugins/MuteTime.lua
+++ plugins/MuteTime.lua
@@ -1,4 +1,4 @@
-﻿--Begin MuteTime.lua By @MahDiRoO
+﻿--Begin MuteTime.lua By @GODILOVEYOUME2
 local function pre_process(msg)
   local hash = 'mute_time:'..msg.chat_id_
   if redis:get(hash) and gp_type(msg.chat_id_) == 'channel' and not is_mod(msg)  then
